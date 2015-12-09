5ddabc5ad0c3c4ee5e329d068a467c21772665a9
lewis-geek <lewis_geek@icloud.com>
2015-12-09 22:00:30 +0800

    add calculate function
    增加了BMI计算和显示功能

--- script/main.js
+++ script/main.js
@@ -10,7 +10,11 @@ $(document).ready(function () {
         $maleIcon = $('.content .fa-male'),
         $age = $('.content .age_info input'),
         $height = $('.content .height_info input'),
-        $weight = $('.content .weight_info input');
+        $weight = $('.content .weight_info input'),
+        $rangeList = $('.range_info li span'),
+        $sexSection = $('.content .age_info .fa'),
+        $result;
+
 
     /*
      菜单栏下拉
@@ -47,11 +51,109 @@ $(document).ready(function () {
 
     /*
      信息处理
+     sexInfo()函数返回1或2
+     1代表female
+     2代表male
      */
+    changeAgeandSexInfo();
+    result();
+
+    function sexInfo() {
+        if ($sexSection.hasClass('fa-female-active')) {
+            return 1;
+        } else if ($sexSection.hasClass('fa-male-active')) {
+            return 2;
+        }
+    }
+
     function changeAgeandSexInfo() {
-        //if
-        //$age.change(function () {
-        //
-        //})
+        $age.change(function () {
+            changeRangeInfo();
+            if (!$age.val()) {
+                $rangeList.eq(0).html('&lt; 0');
+                $rangeList.eq(1).html('0 - 0');
+                $rangeList.eq(2).html('0 - 0');
+                $rangeList.eq(3).html('0 - 0');
+                $rangeList.eq(4).html('&gt; 0');
+            }
+        });
+
+        $sexSection.click(function () {
+            if ($age.val()) {
+                changeRangeInfo();
+            }
+        });
+
+        function changeRangeInfo() {
+            var $sex = sexInfo();
+            if ($sex == 1) {
+                $rangeList.eq(0).html('&lt; 18');
+                $rangeList.eq(1).html('18 - 25');
+                $rangeList.eq(2).html('25 - 30');
+                $rangeList.eq(3).html('30 - 40');
+                $rangeList.eq(4).html('&gt; 40');
+            } else if ($sex == 2) {
+                $rangeList.eq(0).html('&lt; 19');
+                $rangeList.eq(1).html('19 - 25');
+                $rangeList.eq(2).html('25 - 30');
+                $rangeList.eq(3).html('30 - 40');
+                $rangeList.eq(4).html('&gt; 40');
+            }
+        }
+    }
+
+    function result() {
+        monitorChange($age);
+        monitorChange($height);
+        monitorChange($weight);
+        $sexSection.click(function () {
+            calculate();
+        });
+
+        function monitorChange($object) {
+            $object.change(function () {
+                calculate();
+                if ($object == $height) {
+                    idealWeight();
+                }
+            });
+        }
+
+        function calculate() {
+            if ($age.val() && $height.val() && $weight.val() && sexInfo()) {
+                $result = $weight.val() / (($height.val() / 100) * ($height.val() / 100));
+                conuntUp($result);
+                changeSexIcon();
+            }
+        }
+
+        function conuntUp($number) {
+            var options = {
+                useEasing: true,
+                useGrouping: true,
+                separator: ',',
+                decimal: '.',
+                prefix: '',
+                suffix: ''
+            };
+            var demo = new CountUp("resultText", 0, $number, 2, 2, options);
+            demo.start();
+        }
+
+        function changeSexIcon() {
+            var $sex = sexInfo();
+            if ($sex == 1) {
+                $('.result_content i').removeClass().addClass('fa fa-venus')
+            } else if ($sex == 2) {
+                $('.result_content i').removeClass().addClass('fa fa-mars')
+            }
+        }
+
+        function idealWeight() {
+            var $idealWeight = 22 * ($height.val() / 100) * ($height.val() / 100);
+            var $idealWeightMax = Math.round($idealWeight * 1.1);
+            var $idealWeightMin = Math.round($idealWeight * 0.9);
+            $rangeList.eq(5).html($idealWeightMin + ' - ' + $idealWeightMax);
+        }
     }
 });
